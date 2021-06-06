b6a924f12015ffa79528975357dee293a16a95bd
Jayakrishnan <Jayakrishnan@LAPTOP-KPE8IQ28>
2021-06-06 15:29:17 -0700

    Week one

---
+++ LabExercise/src/LabExecise1/AddTwoNumber.java
@@ -0,0 +1,9 @@
+package LabExecise1;
+
+public class AddTwoNumber {
+	public static void main(String[] args) {
+		int number1=100, number2=200, sum;
+		sum=number1+number2;
+		System.out.println("Sum of two number is...:"+sum);
+	}
+	}
